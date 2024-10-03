2bc79d883fb841df31ca7a5cfe93094babedcee5
MuKuSh11 <munesh.developer@gmail.com>
2024-10-03 16:08:33 +0530

    Collapsible reply container

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const commentBtn = document.getElementById("comment-button");
 
 const commentContainer = document.querySelector(".comments-container");
 
+// add click 
 commentBtn.addEventListener("click", () => {
     let commentValue = commentInput.value.trim();
     commentInput.value = "";
@@ -17,20 +18,29 @@ commentContainer.addEventListener("click", (e) => {
     const targetCommentOrReply = clickedBtn.parentElement.parentElement;
     if( clickedBtn.classList.contains("reply-button") ) {
         const replyTextArea = clickedBtn.parentElement.previousElementSibling;
-        console.log(replyTextArea.style.display);
         if( !replyTextArea.style.display ) {
             replyTextArea.style.display = 'inline-block';
         } else {
             const replyValue = replyTextArea.value.trim();
             replyTextArea.value = "";
             if( replyValue ) {
-                addReply(replyValue, targetCommentOrReply)
+                addReply(replyValue, targetCommentOrReply);
             }
             replyTextArea.style.display = '';
         }
     } else if ( clickedBtn.classList.contains("delete-button")) {
-            targetCommentOrReply.remove();
-    }
+        targetCommentOrReply.remove();
+    } else if ( clickedBtn.classList.contains("collapse-button")) {
+        if( clickedBtn.classList.contains("fa-caret-up") ) {
+            clickedBtn.classList.remove("fa-caret-up");
+            clickedBtn.classList.add("fa-caret-down");
+            collapseReplySection(targetCommentOrReply, true);
+        } else {
+            clickedBtn.classList.remove("fa-caret-down");
+            clickedBtn.classList.add("fa-caret-up");
+            collapseReplySection(targetCommentOrReply, false);
+        }
+    };
 });
 
 // Function to add comment in comments-container
@@ -44,6 +54,7 @@ function addComment(commentValue) {
         <div class="comment-btn-container">
             <button class="reply-button">Reply</button>
             <button class="delete-button">Delete</button>
+            <button class="collapse-button fa-solid fa-caret-up"></button>
         </div>
     `;
     commentContainer.appendChild(commentDiv);
@@ -60,7 +71,19 @@ function addReply(replyValue, targetCommentOrReply) {
         <div class="reply-btn-container">
             <button class="reply-button">Reply</button>
             <button class="delete-button">Delete</button>
+            <button class="collapse-button fa-solid fa-caret-up"></button>
         </div>
     `;
     targetCommentOrReply.querySelector(".reply-container").appendChild(replyDiv);
+}
+
+function collapseReplySection(targetCommentOrReply, flag) {
+    const replyContainer = targetCommentOrReply.querySelector(".reply-container");
+    if( replyContainer.hasChildNodes()) {
+        if( flag ) {
+            replyContainer.style.display = "none";
+        } else {
+            replyContainer.style.display = "flex";
+        }
+    }
 }
